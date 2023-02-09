a4b5f9db57d230e2028aadca818cd6f0b3a9bbfa
Noah Vaughn <noahdvaughn@gmail.com>
2023-02-09 13:31:41 -0500

    routed controllers

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,9 @@ const router = Router()
 router.get('/', (req, res) => res.send('This is root!'))
 router.post('/create-player', controllers.createPlayer)
 router.post('/create-opening', controllers.createOpening)
+router.put('/update-player/:id', controllers.updatePlayer)
+router.put('/update-opening/:id', controllers.updateOpening)
+router.delete('/delete-player/:id', controllers.deletePlayer)
+router.delete('/delete-opening/:id', controllers.deleteOpening)
 
 module.exports = router
