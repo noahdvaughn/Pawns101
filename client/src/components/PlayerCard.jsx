3f2e25c85aa1056498e62c5877047af6edd124b4
Noah Vaughn <noahdvaughn@gmail.com>
2023-02-14 13:52:06 -0500

    changed aspect scaling for profile pic

--- client/src/components/PlayerCard.jsx
+++ client/src/components/PlayerCard.jsx
@@ -2,8 +2,8 @@ import {Link} from 'react-router-dom'
 const PlayerCard = ({player}) => {
 
   const PlayerPic = () => {
-    if (player.PlayerPic){
-      return 
+    if (player.profile_url){
+      return <img  className='profilePic'src={`${player.profile_url}`}/>
     } else {
       return <img className='profilePic' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQ_KlnkAkrr10HZoUii1ShdmCDN25m-Ovg7A&usqp=CAU'/>
     }
