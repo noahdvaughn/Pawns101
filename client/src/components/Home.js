a8eef68906e7b78c0f030e0e488fedff522af602
Noah Vaughn <noahdvaughn@gmail.com>
2023-02-15 11:52:38 -0500

    added filter icon

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,6 +4,8 @@ import OpeningCard from './OpeningCard'
 
 const Home = () => {
   const [openingResults, setOpeningResults] = useState([])
+  const [e4Filter, sete4Filter] = useState('False')
+  const [d4Filter, setd4Filter] = useState('False')
 
   useEffect(() => {
     const getAllOpenings = async () => {
@@ -15,17 +17,42 @@ const Home = () => {
     getAllOpenings()
   }, [])
 
+  const MoveFilter = () => {
+    if (e4Filter === 'True') {
+      const e4Results = openingResults.filter(
+        (result) => result.move_list[0] === 'e4'
+      )
+      return e4Results.map((result) => (
+        <OpeningCard key={result._id} opening={result} />
+      ))
+    } else if (d4Filter === 'True') {
+      const d4Results = openingResults.filter(
+        (result) => result.move_list[0] === 'd4'
+      )
+      return d4Results.map((result) => (
+        <OpeningCard key={result._id} opening={result} />
+      ))
+    } else {
+      return openingResults.map((result) => (
+        <OpeningCard key={result._id} opening={result} />
+      ))
+    }
+  }
+
   return (
     <div>
       <div className="topText">
-        <h1 className="pageTitle">Openings</h1>
+        <h1 className="pageTitle">Openings </h1>
+        <img
+          classname="filterIcon"
+          src="https://cdn-icons-png.flaticon.com/512/6488/6488674.png"
+        />
         <h1 className="siteLogo">Pawns ♜o♜</h1>
       </div>
       <div className="homeBody">
-        {openingResults.map((result) => (
-          <OpeningCard key={result._id} opening={result} />
-        ))}
+        <MoveFilter />
       </div>
+      <button>Filter</button>
     </div>
   )
 }
