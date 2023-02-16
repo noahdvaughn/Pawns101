2f2ef6ce79941233d25622db6427246a167158a4
Noah Vaughn <noahdvaughn@gmail.com>
2023-02-16 10:05:44 -0500

    conditionaly rendered elo

--- client/src/components/PlayerCard.jsx
+++ client/src/components/PlayerCard.jsx
@@ -8,11 +8,19 @@ const PlayerCard = ({player}) => {
       return <img className='profilePic' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQ_KlnkAkrr10HZoUii1ShdmCDN25m-Ovg7A&usqp=CAU'/>
     }
   }
+  const DisplayElo = () => {
+    if (player.elo){
+      return <h1 className='thinLetters'>({player.elo})</h1>
+    } else {
+      return
+    }
+  }
 
   return <Link to={`/player-details/${player._id}`} state={{player:player}}className='playerLink'>
     <div className='playerLinkDiv'>
     <PlayerPic/>
-    <h1 className='thinLetters' >{player.name}({player.elo})</h1>
+    <h1 className='thinLetters' >{player.name}</h1>
+    <DisplayElo/>
     </div>
   </Link>
 }
